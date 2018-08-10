72a00f3cff8da47477378d7f6ce3e88b56d19dac
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2018-08-10 13:20:31 +0200

    [bandcamp-radio] First working prototype.

--- bandcamp-radio/bandcamp-radio.js
+++ bandcamp-radio/bandcamp-radio.js
@@ -1,19 +1,24 @@
+'use strict';
+
 // Unify FF and Chrome
 // =======================================================
 let runtime = typeof browser === 'undefined' ? chrome.runtime : browser;
 // =======================================================
 
-function setItem(key, value) {
-	localStorage.setItem(key, JSON.stringify(value));
-	//chrome.storage.local.set({key: value});
+function setItem(key, value, cb) {
+	chrome.storage.local.set({[key]: value}, cb);
 }
 
-function getItem(key) {
-	return JSON.parse(localStorage.getItem(key));
-	// chrome.storage.local.get([key], (value) => {
-	// 	return chrome.runtime.lastError ? undefined : value;
-	// })
+function getItem(key, cb) {
+	chrome.storage.local.get(key, cb);
 }
+
+function removeItem(key, cb) {
+	chrome.storage.local.remove(key, cb);
+}
+
+let radio = {};
+
 
 function navigateToRandomAlbum() {
 	let albumLinks = Object.keys(radio.albums);
@@ -23,12 +28,22 @@ function navigateToRandomAlbum() {
 		albumLinks.splice(albumLinks.indexOf(window.location.href), 1);
 
 	radio.playing = albumLinks[Math.floor(Math.random() * albumLinks.length)];
-	setItem('radio', radio);
-	window.location.href = radio.playing;
+	setItem('radio', radio, () => {
+		window.location.href = radio.playing;
+	});
 }
 
-let radio = getItem('radio') || {};
-let onEndedListener = null;
+function waitForTrackEnd(audioEl) {
+	// Need manual checking to avoid ended event getting triggered
+	// (Which would trigger bandcamp's own handler)
+	if(audioEl.duration - audioEl.currentTime < 1) {
+		audioEl.pause();
+		navigateToRandomAlbum();
+	}
+	else {
+		setTimeout(waitForTrackEnd.bind(null, audioEl), 100);
+	}
+}
 
 function addPlay() {
 	let controls = document.createElement('div');
@@ -42,7 +57,10 @@ function addPlay() {
 		cursor: pointer;
 		background-color:green`;
 
+	controls.innerHTML = "Play";
+
 	controls.addEventListener('click', () => {
+		radio.origin = window.location.href;
 		radio.albums = {};
 
 		let links = document.getElementsByTagName('a');
@@ -53,6 +71,50 @@ function addPlay() {
 		}
 
 		navigateToRandomAlbum();
+	});
+
+	document.body.appendChild(controls);
+}
+
+function addSkip() {
+	let controls = document.createElement('div');
+	controls.style.cssText = `
+		width:40px;
+		height:40px;
+		position:fixed;
+		top: 0px;
+		right: 40px;
+		z-index: 1000;
+		cursor: pointer;
+		background-color:blue`;
+
+	controls.innerHTML = "Skip";
+
+	controls.addEventListener('click', () => {
+		document.getElementsByTagName('audio')[0].pause();
+		navigateToRandomAlbum();
+	});
+
+	document.body.appendChild(controls);
+}
+
+function addResume() {
+	let controls = document.createElement('div');
+	controls.style.cssText = `
+		width:100%;
+		height:100%;
+		position:fixed;
+		top: 0px;
+		left: 0px;
+		z-index: 1000;
+		cursor: pointer;
+		background-color:white`;
+
+	controls.innerHTML = "Resume Playback";
+
+	controls.addEventListener('click', () => {
+		controls.remove();
+		initPlayback();
 	});
 
 	document.body.appendChild(controls);
@@ -70,49 +132,45 @@ function addStop() {
 		cursor: pointer;
 		background-color:red`;
 
+	controls.innerHTML = "Stop";
+
 	controls.addEventListener('click', () => {
-		document.getElementsByTagName('audio')[0].removeEventListener('ended', onEndedListener);
-		radio = {};
-		setItem('radio', radio);
+		removeItem('radio', () => window.location.href = radio.origin);
 	});
 
 	document.body.appendChild(controls);
 }
 
+function initPlayback() {
+	addStop();
+	addSkip();
 
-(() => {
-	'use strict';
+	let currentAlbum = radio.albums[window.location.href];
+	let availableTracks = document.querySelectorAll('.play_status:not(.disabled)');
+	let filteredTracks = availableTracks;
 
-	if(window.location.href === radio.playing) {
-		addStop();
-
-		let currentAlbum = radio.albums[window.location.href];
-		let availableTracks = document.querySelectorAll('.play_status:not(.disabled)');
-		let filteredTracks = availableTracks;
-
-		if(currentAlbum !== null) {
-			// Filter already played tracks
-			if(currentAlbum.played.length > 0) {
-				filteredTracks = availableTracks.filter(
-					(track, idx) => (currentAlbum.player.includes(idx))
-				);
-			}
+	if(currentAlbum !== null) {
+		// Filter already played tracks
+		if(currentAlbum.played.length > 0) {
+			filteredTracks = Array.from(availableTracks).filter(
+				(track, idx) => (!currentAlbum.played.includes(idx))
+			);
 		}
-		else {
-			currentAlbum = {
-				played: []
-			}
-		}
+	}
+	else {
+		currentAlbum = radio.albums[window.location.href] = {
+			played: [],
+		};
+	}
 
+	// Delay playing, otherwise errors because of overlapping pause/play events
+	setTimeout(() => {
 		// PLAY
 		let randomTrack = filteredTracks[Math.floor(Math.random() * filteredTracks.length)];
 		randomTrack.click();
 
 		// Navigate to random album once this track is done
-		let audioEl = document.getElementsByTagName('audio')[0];
-		audioEl.addEventListener('ended', () => {
-			navigateToRandomAlbum();
-		});
+		waitForTrackEnd(document.getElementsByTagName('audio')[0]);
 
 		// Figure out idx of track that is being played and store it
 		availableTracks.forEach((track, idx) => {
@@ -124,11 +182,30 @@ function addStop() {
 		if(currentAlbum.played.length >= availableTracks.length) {
 			delete radio.albums[window.location.href];
 		}
-	}
-	else {
-		// At least two album links were found
-		if(document.body.innerHTML.indexOf("/album/") < document.body.innerHTML.lastIndexOf("/album/")) {
-			addPlay();
+
+		setItem('radio', radio);
+	}, 500);
+}
+
+(() => {
+	getItem('radio', (values) => {
+		if(chrome.runtime.lastError || Object.keys(values).length === 0) {
+			// At least two album links were found
+			if(document.body.innerHTML.indexOf("/album/") < document.body.innerHTML.lastIndexOf("/album/")) {
+				addPlay();
+			}
 		}
-	}
+		else {
+			radio = values.radio;
+
+			if(window.location.href === radio.playing) {
+				if(document.referrer === "") {
+					addResume();
+				}
+				else {
+					initPlayback();
+				}
+			}
+		}
+	});
 })();
